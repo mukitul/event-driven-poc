63c7d145ee33d3e2ea9110bf38afc7e6b53ea421
mukitul <mukitul.exabyting@bkash.com>
2024-10-09 10:44:09 +0600

    first commit

---
+++ src/main/java/com/mukit/eventdrivenpoc/EventDrivenPocApplication.java
@@ -0,0 +1,13 @@
+package com.mukit.eventdrivenpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenPocApplication.class, args);
+    }
+
+}
